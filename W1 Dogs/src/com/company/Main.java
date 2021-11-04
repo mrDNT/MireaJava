5a88d20fe4b6817a7be88af0000c9ef1b04016db
morodont <demid.driga@mail.ru>
2021-11-05 01:54:14 +0300

    Work 1.1 Dogs
    woof

---
+++ W1 Dogs/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog first_dog = new Dog("Spark", 1);
+        Dog second_dog = new Dog(2);
+        System.out.println(first_dog.toString());
+        System.out.println(second_dog.toString());
+    }
+}
