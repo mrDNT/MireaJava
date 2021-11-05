77fca6bf8a5aaedc5581f6d2a755f04cf7f29699
morodont <demid.driga@mail.ru>
2021-11-06 02:15:30 +0300

    Work 5 (made in swing GUI editor)

---
+++ W5/src/GUI/Layout.java
@@ -0,0 +1,46 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Layout extends JFrame{
+    int milan;
+    int madrid;
+    private JButton ACMilanButton;
+    private JButton realMadridButton;
+    private JLabel result;
+    private JLabel last;
+    private JLabel winner;
+    private JPanel rootPanel;
+
+    public void labelTick(){
+        result.setText("Result: " + milan + " x " + madrid);
+        if (milan > madrid){
+            winner.setText("Winner: AC Milan");
+        }
+        else if (milan == madrid){
+            winner.setText("Winner: Draw");
+        }
+        else{
+            winner.setText("Winner: Real Madrid");
+        }
+    }
+    public Layout() {
+        setContentPane(rootPanel);
+        setVisible(true);
+        setSize(600, 600);
+        realMadridButton.addActionListener((event) -> {
+                madrid++;
+                labelTick();
+                last.setText("Last scorer: Real Madrid");
+        });
+        ACMilanButton.addActionListener((event) -> {
+                milan++;
+                labelTick();
+                last.setText("Last scorer: AC Milan");
+        });
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
---
+++ W5/src/GUI/Main.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        new Layout();
+
+    }
+}
